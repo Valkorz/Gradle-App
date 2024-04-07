517c170a4c5ad13a4db518d402c1022d72b79099
Valkorz <pivarcivittorio@gmail.com>
2024-04-07 01:26:13 -0300

    Added in 01:26AM 07/04/2024

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a project that creates an interaction between C/C++ and Java inside a Gradle application.
+
+Dependencies:
+- JDK;
+- GRADLE;
+- GNU/GCC;
---
+++ app/src/com/java/root/App.java
@@ -0,0 +1,9 @@
+package src.com.java.root;
+import javafx.application;
+
+public class App extends Application {
+    public static void main (String[] args){
+        
+    }
+    
+}
